a63470660808aa52720254b2744ff9000a9f5571
tgrosh <tgrosh@yahoo.com>
2021-02-19 11:06:34 -0500

    Collector will collect and deliver resources

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,24 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.VFX;
 
 public class Collector : MonoBehaviour
 {
-    public VisualEffect collectorEffect;
-    public float collectionRange;
-    public float collectionPerSecond;
-    public int currentlyCollectedResources;
     public int maxResources;
+    public float collectionPerSecond;
+    public float collectionRange;
+    public float deliveryRange;
+    public VisualEffect collectorEffect;
+    public int currentlyCollectedResources;
 
     Mover mover;
-    bool atResourceTarget;
-    bool isDelivering;
-    ResourceNode resourceTarget;
     Targeter targeter;
+
+    bool atResourceTarget;
+    bool atDeliveryTarget;
+
+    Base deliveryTarget; // the place i will deliver to
+    ResourceNode resourceTarget; // the place i will collect from
+
     float collectionTimer;
-    Base nearestBase;
 
     // Start is called before the first frame update
     void Start()
@@ -32,47 +33,83 @@ public class Collector : MonoBehaviour
     {
         if (targeter.target)
         {
-            resourceTarget = targeter.target.GetComponent<ResourceNode>();            
-        }
-
-        //if we have a resource target, and we are not full, and we are not returning to base to deliver
-        if (resourceTarget && !atResourceTarget && currentlyCollectedResources < maxResources && !isDelivering)
-        {
-            //move to resource node and collect
-            mover.SetDestination(resourceTarget.transform.position);
+            resourceTarget = targeter.target.GetComponent<ResourceNode>();
         }
 
         atResourceTarget = mover.moveComplete && resourceTarget && Vector3.Distance(transform.position, resourceTarget.transform.position) < collectionRange;
+        atDeliveryTarget = mover.moveComplete && deliveryTarget && Vector3.Distance(transform.position, deliveryTarget.transform.position) < deliveryRange;
 
-        if (atResourceTarget)
+        //if we have a resource target, and we are not at the resource, and we are not full
+        if (resourceTarget && !atResourceTarget && CanCollectFromResource(resourceTarget))
         {
-            transform.LookAt(new Vector3(resourceTarget.transform.position.x, transform.position.y, resourceTarget.transform.position.z));
-            if (collectionPerSecond > 0 && collectionTimer > 1/collectionPerSecond)
-            {
-                int collectionAmount = resourceTarget.resourcesPerCollect;
-                if (currentlyCollectedResources <= maxResources - collectionAmount)
-                {
-                    resourceTarget.Collect();
-                    currentlyCollectedResources += collectionAmount;
-                } else
-                {
-                    //go back to base
-                    Transform nearestBaseTransform = FindNearestBase();
-                    if (nearestBaseTransform != null)
-                    {
-                        nearestBase = nearestBaseTransform.gameObject.GetComponent<Base>();
-                        mover.SetDestination(nearestBase.transform.position);
-                    }                    
-                }
-                collectionTimer = 0f;
-            }
-            collectionTimer += Time.deltaTime;
-
-            collectorEffect.gameObject.SetActive(true);
-        } else
-        {
-            collectorEffect.gameObject.SetActive(false);
+            //move to resource node
+            mover.SetDestination(resourceTarget.transform.position);
         }
+
+        //if we have a resource target, and we are at the resource, and we are not full
+        if (resourceTarget && atResourceTarget && CanCollectFromResource(resourceTarget))
+        {
+            //collect
+            Collect();
+        }
+
+        //if we have a resource target, and we are full, and we are not at delivery target
+        if (resourceTarget && !CanCollectFromResource(resourceTarget) && !atDeliveryTarget)
+        {
+            //move to delivery target
+            if (!deliveryTarget)
+            {
+                deliveryTarget = FindNearestBase().GetComponent<Base>();
+            }
+            mover.SetDestination(deliveryTarget.transform.position);
+        }
+
+        //if we dont have a resource target, and we have any resources, and we are not at delivery target
+        if (!resourceTarget && currentlyCollectedResources > 0 && !atDeliveryTarget)
+        {
+            //move to delivery target
+            if (!deliveryTarget)
+            {
+                deliveryTarget = FindNearestBase().GetComponent<Base>();
+            }
+            mover.SetDestination(deliveryTarget.transform.position);
+        }
+
+        //if we have a delivery target, and we are at the delivery target, and we have resources to deliver
+        if (deliveryTarget && atDeliveryTarget && currentlyCollectedResources > 0)
+        {
+            //deliver
+            Deliver();
+        }
+
+        collectorEffect.gameObject.SetActive(resourceTarget && atResourceTarget && CanCollectFromResource(resourceTarget));
+    }
+
+    bool CanCollectFromResource(ResourceNode resource)
+    {
+        return currentlyCollectedResources <= maxResources - resource.resourcesPerCollect;
+    }
+
+    void Collect()
+    {
+        transform.LookAt(new Vector3(resourceTarget.transform.position.x, transform.position.y, resourceTarget.transform.position.z));
+        if (collectionPerSecond > 0 && collectionTimer > 1 / collectionPerSecond)
+        {
+            int collectionAmount = resourceTarget.resourcesPerCollect;
+            if (CanCollectFromResource(resourceTarget))
+            {
+                resourceTarget.Collect();
+                currentlyCollectedResources += collectionAmount;
+            }
+            collectionTimer = 0f;
+        }
+        collectionTimer += Time.deltaTime;
+    }
+
+    void Deliver()
+    {
+        EventManager.Emit(EventManager.Events.ResourceCollected, currentlyCollectedResources);
+        currentlyCollectedResources = 0;
     }
 
     Transform FindNearestBase()

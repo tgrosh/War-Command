7a6427522a62f971964a76a80784e035d42a2fa2
tgrosh <tgrosh@yahoo.com>
2021-02-19 12:14:31 -0500

    Targeter only shows marker when selected

--- Assets/Scripts/Targeter.cs
+++ Assets/Scripts/Targeter.cs
@@ -11,41 +11,55 @@ public class Targeter : MonoBehaviour
 
     NavMeshAgent agent;
     GameObject marker;
+    Selectable selectable;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        selectable = GetComponent<Selectable>();
     }
 
     void Update()
     {
-        
+        if (target && selectable && selectable.isSelected)
+        {
+            ShowMarker();
+        } else
+        {
+            ClearMarker();
+        }
     }
 
     public void SetTarget(Transform targetTransform)
     {
         target = targetTransform;
-        ShowMarker();
     }
 
-    void ShowMarker()
+    public void ShowMarker()
     {
         if (marker)
         {
             Destroy(marker);
         }
+
+        if (!target) return;
         marker = Instantiate(targetMarkerPrefab, new Vector3(target.position.x, 0, target.position.z), Quaternion.identity);
         marker.GetComponentInChildren<MeshRenderer>().material.color = Color.green;
         marker.transform.localScale = Vector3.one * target.GetComponent<Collider>().bounds.extents.magnitude;
     }
 
-    public void ClearTarget()
+    void ClearMarker()
     {
-        target = null;
         if (marker)
         {
             Destroy(marker);
         }
+    }
+
+    public void ClearTarget()
+    {
+        target = null;
+        ClearMarker();
     }
 }
